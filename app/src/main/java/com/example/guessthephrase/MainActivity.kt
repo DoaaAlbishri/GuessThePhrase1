bc5776197c0e03af07b8efc3a953120329d9ec6e
Doaa <Duaa_ali@outlook.sa>
2021-10-02 15:29:18 +0300

    Second

--- app/src/main/java/com/example/guessthephrase/MainActivity.kt
+++ app/src/main/java/com/example/guessthephrase/MainActivity.kt
@@ -9,101 +9,112 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var editText : EditText
+    lateinit var button : Button
+    lateinit var myLayout : ConstraintLayout
+    lateinit var myRv : RecyclerView
+    lateinit var tv1 : TextView
+
+    var pharse = "Hello World"
+    var str = "*".repeat(pharse.length)
+    var Newstr = str
+    var left = 10
+    var letter = 10
+    var guessList = arrayListOf<String>()
+    var leftList = arrayListOf<String>()
+    var letterList = arrayListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var myLayout = findViewById<ConstraintLayout>(R.id.clMain)
-        val tv1 = findViewById<TextView>(R.id.tv)
-        val tv2 = findViewById<TextView>(R.id.tv1)
-        val tv3 = findViewById<TextView>(R.id.tv2)
-        val tv4 = findViewById<TextView>(R.id.tv3)
-        val button = findViewById<Button>(R.id.button)
-        val editText = findViewById<EditText>(R.id.editText)
-
-
-        var pharse = "Hello World"
-        var str = "*".repeat(pharse.length)
-        //var str=""
-        var Newstr = str
-        var left = 10
-        var letter = 10
-
-/*
-        for (i in 0..pharse.length - 1) {
-            if (pharse[i].equals("\\s")){
-                continue
-            }else{
-               str= pharse.replace(pharse[i],'*')
-            }
-        }
-*/
-
+        myLayout = findViewById(R.id.clMain)
+        tv1 = findViewById(R.id.tv)
+        myRv = findViewById(R.id.recyclerView)
+        button = findViewById(R.id.button)
+        editText = findViewById(R.id.editText)
 
         tv1.setText("Pharse: ${str} \n Gussed Letter: ")
 
+        myRv.adapter = RecyclerViewAdapter(guessList, leftList, letterList)
+        myRv.layoutManager = LinearLayoutManager(this)
+
         button.setOnClickListener {
-                if (left > 0) {
-                    var input = editText.text.toString()
-                    if (!editText.text.isEmpty()) {
-                        if (pharse.toLowerCase() == input.toLowerCase()) {
-                            --left
-                            tv1.setText("Pharse: ${input} \n Gussed Letter: ")
-                            tv4.setText("${left} guesses remaining")
-                            this.recreate()
-                        } else {
-                            --left
-                            tv2.setText("Wrong guess : ${input}")
-                            tv4.setText("${left} guesses remaining")
-                        }
-                    }
-                    if(left==0){
-                        editText.hint = "Gusses the letter"
-                    }
-                }
-                else if (letter>0 && left<=0){
-                    editText.hint = "Gusses the letter"
-                    var input:Char = editText.text.toString()[0]
-                    var count = 0
-                    if (!editText.text.isEmpty()&&editText.text.length==1) {
-                        letter--
-                        for (i in 0..pharse.length - 1) {
-                            if (pharse[i].toLowerCase().equals(input.toLowerCase())) {
-                                var chars =Newstr.toCharArray()
-                                chars[i]=input
-                                Newstr = String(chars)
-                               // Newstr = Newstr.replace(Newstr[i], input)
-                                count++
-                        }
-                            if(pharse[i] == ' '){
-                                var chars =Newstr.toCharArray()
-                                chars[i]=' '
-                                Newstr = String(chars)
-                            }
-                    }
-                        if(count>0){
-                            tv1.setText("Pharse: ${Newstr} \n Gussed Letter: ${input} ")
-                            tv2.setText("Wrong guess : ")
-                            tv3.setText("Founds: ${count} ${input.toUpperCase()}(s)")
-                            tv4.setText("${letter} guesses remaining")
-                        } else {
-                            tv2.setText("Wrong guess : ${input}")
-                            tv4.setText("${letter} guesses remaining")
-                        }
-                        }else{
-                        Snackbar.make(myLayout,"Enter one letter please",Snackbar.LENGTH_LONG).show()
-                    }
-                    if(Newstr.equals(pharse,true)||letter==0){
-                        this.recreate()
-                    }
-                }
+            pharse()
             editText.text.clear()
 
         }
-
-
     }
-}
+
+    private fun pharse() {
+        if (left > 0) {
+            var input = editText.text.toString()
+            if (!editText.text.isEmpty()) {
+                if (pharse.toLowerCase() == input.toLowerCase()) {
+                    --left
+                    tv1.setText("Pharse: ${input} \n Gussed Letter: ")
+                    guessList.add("")
+                    letterList.add("")
+                    leftList.add("${left} guesses remaining")
+                    myRv.adapter?.notifyDataSetChanged()
+                    this.recreate()
+                } else {
+                    --left
+                    guessList.add("Wrong guess : ${input}")
+                    leftList.add("${left} guesses remaining")
+                    letterList.add("")
+                    myRv.adapter?.notifyDataSetChanged()
+                }
+                if (left == 0) {
+                    editText.hint = "Gusses the letter"
+                }
+            }
+        } else {
+                letter()
+        }
+    }
+
+    private fun letter(){
+            editText.hint = "Gusses the letter"
+            var input: Char = editText.text.toString()[0]
+            var count = 0
+            if (!editText.text.isEmpty() && editText.text.length == 1) {
+                letter--
+                for (i in 0..pharse.length - 1) {
+                    if (pharse[i].toLowerCase().equals(input.toLowerCase())) {
+                        var chars = Newstr.toCharArray()
+                        chars[i] = input
+                        Newstr = String(chars)
+                        // Newstr = Newstr.replace(Newstr[i], input)
+                        count++
+                    }
+                    if (pharse[i] == ' ') {
+                        var chars = Newstr.toCharArray()
+                        chars[i] = ' '
+                        Newstr = String(chars)
+                    }
+                }
+                if (count > 0) {
+                    tv1.setText("Pharse: ${Newstr} \n Gussed Letter: ${input} ")
+                    guessList.add("Wrong guess : ")
+                    letterList.add("Founds: ${count} ${input.toUpperCase()}(s)")
+                    leftList.add("${letter} guesses remaining")
+                    myRv.adapter?.notifyDataSetChanged()
+                } else {
+                    guessList.add("Wrong guess : ${input}")
+                    letterList.add("")
+                    leftList.add("${letter} guesses remaining")
+                    myRv.adapter?.notifyDataSetChanged()
+                }
+            } else {
+                Snackbar.make(myLayout, "Enter one letter please", Snackbar.LENGTH_LONG).show()
+            }
+            if (Newstr.equals(pharse, true) || letter == 0) {
+                this.recreate()
+            }
+        }
+    }
